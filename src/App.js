f8f1b1fc6ab2c42f15e9a6af1e5e6b041c21056c
Shane M <shaneSC@gmail.com>
2019-11-17 11:21:39 +0000

    added state to App container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { data } from './cards.js';
 
-const App = () => {
-  return (
-    <div>
-      <CardList cards={data} />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: data
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <CardList cards={this.state.cards} />
+      </div>
+    );
+  }
 }
 
 export default App;
